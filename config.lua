cc990a1d83a361604b93329ce2033b500fa55ec3
creative2117 <sebbe99j@hotmail.com>
2023-04-13 22:41:23 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,16 +1,19 @@
 Config = {
     debug = false,
-    useTarget = true, -- GetConvar('UseTarget', 'false') == 'true',
+    useTarget = GetConvar('UseTarget', 'false') == 'true',
 
     key = 47, -- (G) https://docs.fivem.net/docs/game-references/controls/
     
-    textOnDuty = "Go off duty",
-    textOffDuty = "Go on duty",
-    targetText = "Toggle duty",
-    targetDontHaveJob = "You dont have this job",
+    textOnDuty = "Go off duty", -- 3d text to display when you are ON duty
+    textOffDuty = "Go on duty", -- 3d text to display when you are OFF duty
+    targetText = "Toggle duty", -- Text to display in qb-target
+    targetDontHaveJob = "You dont have this job", -- what to notify when you don't have the job when useing target
     
+    --name = name of the job
+    --coords = coords for the location
+    --icon = the icon in qb-target
     jobLocations = {
-        ["police"] = { name = "police", coords = vector3(901.46, -1060.14, 32.83 - 1), icon = "fa fa-clipboard" },
+        ["police"] = { name = "police", coords = vector3(901.46, -1060.14, 32.83), icon = "fa fa-clipboard" },
         ["ambulance"] = { name = "ambulance", coords = vector3(896.97, -1057.59, 32.83), icon = "fa fa-clipboard" },
     },
 }
